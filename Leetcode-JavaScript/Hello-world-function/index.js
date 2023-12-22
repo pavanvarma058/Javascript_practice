1869be03157d5c8419ed6884899f7ca2e40437cd
Pavan Varma <124238117+pavanvarma058@users.noreply.github.com>
2023-12-22 23:34:28 +0530

    Delete Leetcode-JavaScript/Hello-world-function directory

--- Leetcode-JavaScript/Hello-world-function/index.js
+++
@@ -1,8 +0,0 @@
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
-    }
-};
