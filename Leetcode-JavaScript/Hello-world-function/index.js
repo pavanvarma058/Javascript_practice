fe4ecb8ccf7267dda22c8f2291d55d569c0eceb0
pavanvarma058 <pavanvarma058@gmail.com>
2023-12-24 13:01:29 +0530

    commiting local changes

--- Leetcode-JavaScript/Hello-world-function/index.js
+++ Leetcode-JavaScript/Hello-world-function/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 
 var createHelloWorld = function() {
@@ -5,4 +6,13 @@ var createHelloWorld = function() {
     return function(...args) {
         return "Hello World";
     }
+=======
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+>>>>>>> cd7b4d2173b01a739a10318e96274438bc2d8b2b
 };
--- Leetcode-JavaScript/counter/index.js
+++ Leetcode-JavaScript/counter/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Given an integer n, return a counter function. This counter function initially returns n and
  then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc). */
 
@@ -7,3 +8,14 @@
         return n++;
     };
 };
+=======
+/* Given an integer n, return a counter function. This counter function initially returns n and
+ then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc). */
+
+ var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+>>>>>>> cd7b4d2173b01a739a10318e96274438bc2d8b2b
