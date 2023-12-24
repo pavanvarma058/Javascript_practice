4c64ea59a64af746c7038bf024114ad6e106b4a9
pavanvarma058 <pavanvarma058@gmail.com>
2023-12-24 13:52:48 +0530

    hello-world function modified

--- Leetcode-JavaScript/Hello-world-function/index.js
+++ Leetcode-JavaScript/Hello-world-function/index.js
@@ -1,28 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
-    }
-=======
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
-    }
->>>>>>> cd7b4d2173b01a739a10318e96274438bc2d8b2b
-=======
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
-    }
->>>>>>> cd7b4d2173b01a739a10318e96274438bc2d8b2b
-};
+// Write a function createHelloWorld. It should return a new function that a
