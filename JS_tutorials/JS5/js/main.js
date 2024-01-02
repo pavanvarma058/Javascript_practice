08bc6d2c176ef8699e0cd911d7c95cc05fdf5cf6
pavanvarma058 <pavanvarma058@gmail.com>
2024-01-02 10:31:19 +0530

    new files added with more practice

---
+++ JS_tutorials/JS5/js/main.js
@@ -0,0 +1,32 @@
+// Math methods and properties
+
+console.log(Math.PI);
+
+// Math.trunc method returns the number without any decimal digits
+console.log(Math.trunc(Math.PI));
+
+// Math.round returns the numebr with the nearest integer
+console.log(Math.round(Math.PI));
+
+// Math.ceil returns the rounded value no matter which value we given
+console.log(Math.ceil(3.45));
+
+// Math.floor returns the numebr with rounded downed value
+console.log(Math.floor(3.56));
+
+// Math.pow method return the power of the value like 2^5
+console.log(Math.pow(2, 10));
+
+// Math.min returns the lowest value in an given numbers
+console.log(Math.min(2, 5, 1));
+
+// Math.max returns the largest number 
+console.log(Math.max(2, 5, 8));
+
+// Math.random returns the random decimal numbers
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1); // returns the random number in digits
+
+
+
+
