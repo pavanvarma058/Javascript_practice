b65e461e8668ac9bc59336c4cdb90c588e78f2d3
pavanvarma058 <pavanvarma058@gmail.com>
2024-01-02 11:59:47 +0530

    tenatary operators example added

--- JS_tutorials/JS8/js/main.js
+++ JS_tutorials/JS8/js/main.js
@@ -1,7 +1,40 @@
-// write javascript program that returns a random letter from your name
+// conditionals: Ternary operator
 
-let myName = "Yeswanth";
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+// syntax
+//condition ? exprIfTrue : exprIfFalse
+let soup = "chicken soup";
+let isCusomerBanned = false;
+let soupAccess = isCusomerBanned ? "sorry, no soup for you" : soup ? `Yes, we have ${soup}` : "Sorry, we don't have soup";
+console.log(soupAccess);
+
+// testscore example
+let testScore = 89;
+let myGrade =
+  testScore > 89
+    ? "A"
+    : testScore > 79
+      ? "B"
+      : testScore > 69
+        ? "C"
+        : testScore > 59
+          ? "D"
+          : "F";
+console.log(`My test grade is a ${myGrade}`);
+
+// rock paper and scissor example
+let PlayerOne = "rock";
+let computer = "rock";
+let result =
+  PlayerOne === computer
+    ? "Tie"
+    : PlayerOne === "rock" && computer === "paper"
+      ? "Computer wins"
+      : playerOne === "paper" && computer === "scissors"
+        ? "Computer wins"
+        : playerOne === "scissors" && computer === "rock"
+          ? "Computer wins"
+          : "Player wins";
+console.log(result);
 
 
 
