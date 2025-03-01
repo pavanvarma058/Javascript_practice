a6464ef5729b67446532c009a2dcd0d34649167d
Pavan Varma <pavanvarma058@gmail.com>
2025-03-02 01:58:25 +0530

    added another exercise to the data types section

--- JS-assignments/Fundamentals-Part-1/dataTypes.js
+++ JS-assignments/Fundamentals-Part-1/dataTypes.js
@@ -15,3 +15,20 @@ console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof country);
 console.log(typeof population);
+
+/*
+let, const and var﻿
+1.Set the value of language to the language spoken 
+where you live (some countries have multiple languages, but just choose one).
+
+2.Think about which variables should be const variables (which values will never change, and which might change?). 
+Then, change these variables to const.
+
+3.Try to change one of the changed variables now, and observe what happens.
+*/
+
+language = "Telugu";
+const count = "India";
+count = "USA"; // Uncaught TypeError: Assignment to constant variable.
+console.log(count); // Uncaught ReferenceError: count is not defined
+console.log(language); // Telugu
