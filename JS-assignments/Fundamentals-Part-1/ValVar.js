25b5e7ef43560cd4bb267c0c30f54fda41c32fbe
Pavan Varma <pavanvarma058@gmail.com>
2025-03-01 23:40:59 +0530

    started solving assignments given in jonas udemy course

---
+++ JS-assignments/Fundamentals-Part-1/ValVar.js
@@ -0,0 +1,14 @@
+/*
+1.Declare variables called country, continent and population and assign their values 
+according to your own country (population in millions).
+
+2.Log their values to the console.
+
+*/
+let country = "India";
+let continent = "Asia";
+let population = 1350;
+
+console.log(country);
+console.log(continent);
+console.log(population);
